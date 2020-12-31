6dfffd1717a95171f2116fa1c49dd568f0ea1c1f
Panic <panic.milos99@gmail.com>
2020-12-31 21:14:15 +0100

    fix: serializing type of class.

--- src/core/domain/models/User.java
+++ src/core/domain/models/User.java
@@ -2,6 +2,9 @@ package core.domain.models;
 
 import java.time.LocalDateTime;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 
@@ -10,6 +13,13 @@ import core.domain.enums.UserRole;
 import core.utils.JsonDateDeserializer;
 import core.utils.JsonDateSerializer;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY)
+@JsonSubTypes({
+	  @JsonSubTypes.Type(value = Buyer.class, name = "Buyer"),
+	  @JsonSubTypes.Type(value = Seller.class, name = "Seller"),
+	  @JsonSubTypes.Type(value = Administrator.class, name = "Administrator")
+})
 public abstract class User extends BaseEntity {
 	private String Username;
 	private String Password;
