9d80756cf19d1a8f11a15ab7610af593d7997219
Zoran Jankov <zoranjankov99@gmail.com>
2021-01-02 15:52:25 +0100

    feat: added more data to seeder

--- src/seeders/Seeder.java
+++ src/seeders/Seeder.java
@@ -2,8 +2,12 @@ package seeders;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.UUID;
 
 import core.domain.enums.Gender;
+import core.domain.enums.ManifestationType;
+import core.domain.enums.TicketStatus;
+import core.domain.enums.TicketType;
 import core.domain.models.Address;
 import core.domain.models.Administrator;
 import core.domain.models.Buyer;
@@ -17,16 +21,16 @@ import repository.Repository;
 import repository.UserRepository;
 
 public class Seeder {
-	
+
 	private DbContext context;
-	
+
 	public Seeder(DbContext context) {
 		this.context = context;
 	}
-	
+
 	public void run() {
-		
-		
+
+		// Locations
 		Repository<Location> locationRepository = new Repository<>(context, Location.class);
 		Location location1 = new Location("45.2470805144055", "19.845354677817618",
 				new Address("Sutjeska", "2", " Novi Sad", "21000"));
@@ -36,61 +40,125 @@ public class Seeder {
 				new Address("Bulevar Arsenija Carnojevica", " 58", "Beograd", "11070"));
 		Location location4 = new Location("45.255476227223596", "9.861740782873927",
 				new Address("Vinogradska", "47", "Novi Sad", "21132"));
-		
+
 		locationRepository.create(location1);
 		locationRepository.create(location2);
 		locationRepository.create(location3);
 		locationRepository.create(location4);
-		
-		//TODO: za datume je potrebno koristiti DateTimeFormatter
-		UserRepository userRepository = new UserRepository(context);
-		
-		Administrator admin1 = new Administrator("admin", "admin", "Mirko", "Mirkovic", Gender.Male, LocalDateTime.now());
-		Administrator admin2 = new Administrator("djumbir", "cajodistog", "Djakic", "Markovic", Gender.Male, LocalDateTime.now());
 
-		userRepository.create(admin1);
-		userRepository.create(admin2);
-		
+		// BuyerTypes
 		Repository<BuyerType> buyerTypeRepository = new Repository<>(context, BuyerType.class);
 		BuyerType goldType = new BuyerType("Gold", 10, 2000);
 		BuyerType silverType = new BuyerType("Silver", 5, 1500);
 		BuyerType bronzeType = new BuyerType("Bronze", 3, 1000);
 		BuyerType defaultType = new BuyerType("Default", 0, 0);
-		
+
 		goldType = buyerTypeRepository.create(goldType);
 		silverType = buyerTypeRepository.create(silverType);
 		bronzeType = buyerTypeRepository.create(bronzeType);
 		defaultType = buyerTypeRepository.create(defaultType);
-		
-		Buyer buyer1 = new Buyer("paneze", "panic", "Milos", "Panic", Gender.Male,
-				LocalDateTime.now(), new ArrayList<Ticket>(), 100, defaultType.getId(), defaultType);
-		Buyer buyer2 = new Buyer("majoneze", "huawei_ruter", "Dalibor", "Malic", Gender.Male,
-				LocalDateTime.now(), new ArrayList<Ticket>(), 1000, bronzeType.getId(), bronzeType);
-		Buyer buyer3 = new Buyer("sljuxa", "markocar123", "Marko", "Suljak", Gender.Male,
-				LocalDateTime.now(), new ArrayList<Ticket>(), 1800, silverType.getId(), silverType);
-		Buyer buyer4 = new Buyer("nadxa", "harry_styles", "Nadezda", "Seratlic", Gender.Female,
-				LocalDateTime.now(), new ArrayList<Ticket>(), 1800, goldType.getId(), goldType);
-		Buyer buyer5 = new Buyer("zocalez", "patos", "Zoran", "Jankov", Gender.Male,
-				LocalDateTime.now(), new ArrayList<Ticket>(), 500, defaultType.getId(), defaultType);
-		
+
+		// Administrators
+		UserRepository userRepository = new UserRepository(context);
+
+		Administrator admin1 = new Administrator("admin", "admin", "Mirko", "Mirkovic", Gender.Male,
+				LocalDateTime.now());
+		Administrator admin2 = new Administrator("djumbir", "cajodistog", "Djakic", "Markovic", Gender.Male,
+				LocalDateTime.now());
+
+		userRepository.create(admin1);
+		userRepository.create(admin2);
+
+		// Buyers
+		Buyer buyer1 = new Buyer("paneze", "panic", "Milos", "Panic", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Ticket>(), 100, defaultType.getId(), defaultType);
+		Buyer buyer2 = new Buyer("majoneze", "huawei_ruter", "Dalibor", "Malic", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Ticket>(), 1000, bronzeType.getId(), bronzeType);
+		Buyer buyer3 = new Buyer("sljuxa", "markocar123", "Marko", "Suljak", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Ticket>(), 1800, silverType.getId(), silverType);
+		Buyer buyer4 = new Buyer("nadxa", "harry_styles", "Nadezda", "Seratlic", Gender.Female, LocalDateTime.now(),
+				new ArrayList<Ticket>(), 2800, goldType.getId(), goldType);
+		Buyer buyer5 = new Buyer("zocalez", "patos", "Zoran", "Jankov", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Ticket>(), 500, defaultType.getId(), defaultType);
+
 		userRepository.create(buyer1);
 		userRepository.create(buyer2);
 		userRepository.create(buyer3);
 		userRepository.create(buyer4);
 		userRepository.create(buyer5);
-		
-		Seller seller1 = new Seller("skc", "skc123", "Simfonije", "Gvozdimirovic",
-				Gender.Male, LocalDateTime.now(), new ArrayList<Manifestation>());
-		Seller seller2 = new Seller("spens", "spens021", "Kavkaz", "Majovic",
-				Gender.Male, LocalDateTime.now(), new ArrayList<Manifestation>());
-		Seller seller3 = new Seller("jeftin_exit", "exit021", "Glorija", "Petrov",
-				Gender.Female, LocalDateTime.now(), new ArrayList<Manifestation>());
-		
+
+		// Sellers
+		Seller seller1 = new Seller("skc", "skc123", "Simfonije", "Gvozdimirovic", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Manifestation>());
+		Seller seller2 = new Seller("spens", "spens021", "Kavkaz", "Majovic", Gender.Male, LocalDateTime.now(),
+				new ArrayList<Manifestation>());
+		Seller seller3 = new Seller("jeftin_exit", "exit021", "Glorija", "Petrov", Gender.Female, LocalDateTime.now(),
+				new ArrayList<Manifestation>());
+
+		// Manifestations
+		Repository<Manifestation> manifestationRepository = new Repository<>(context, Manifestation.class);
+
+		Manifestation manifestation1 = new Manifestation("MF DOOM Memorial", ManifestationType.Concert, 5000,
+				LocalDateTime.now(), 1000, true, location1.getId(), location1, seller1.getId(), seller1);
+		Manifestation manifestation2 = new Manifestation("Bajaga i instuktori, previse godina",
+				ManifestationType.Concert, 500, LocalDateTime.now(), 400, true, location2.getId(), location2,
+				seller2.getId(), seller2);
+		Manifestation manifestation3 = new Manifestation("Ujka Vanja", ManifestationType.Theater, 200,
+				LocalDateTime.now(), 800, true, location4.getId(), location4, seller2.getId(), seller2);
+		Manifestation manifestation4 = new Manifestation("Exit Festival", ManifestationType.Theater, 10000,
+				LocalDateTime.now(), 1400, true, location3.getId(), location3, seller3.getId(), seller3);
+
+		manifestationRepository.create(manifestation1);
+		manifestationRepository.create(manifestation2);
+		manifestationRepository.create(manifestation3);
+		manifestationRepository.create(manifestation4);
+
+		seller1.getManifestations().add(manifestation1);
+		seller2.getManifestations().add(manifestation2);
+		seller2.getManifestations().add(manifestation3);
+		seller3.getManifestations().add(manifestation4);
+
 		userRepository.create(seller1);
 		userRepository.create(seller2);
 		userRepository.create(seller3);
 
+		// Tickets
+		Repository<Ticket> ticketRepository = new Repository<>(context, Ticket.class);
+
+		Ticket ticket1 = new Ticket("c8cENsRLNO", manifestation1.getId(), manifestation1, manifestation1.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer1.getId(), buyer1, TicketStatus.Reserved,
+				TicketType.Regular);
+		Ticket ticket2 = new Ticket("KeVXbpB9LW", manifestation1.getId(), manifestation3, manifestation3.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer2.getId(), buyer2, TicketStatus.Reserved,
+				TicketType.Regular);
+		Ticket ticket3 = new Ticket("gKane57z4T", manifestation1.getId(), manifestation3, manifestation3.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer3.getId(), buyer3, TicketStatus.Canceled,
+				TicketType.Regular);
+		Ticket ticket4 = new Ticket("8O6VymZCSg", manifestation1.getId(), manifestation2, manifestation2.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer3.getId(), buyer3, TicketStatus.Reserved,
+				TicketType.Regular);
+		Ticket ticket5 = new Ticket("rByJcHHPGk", manifestation1.getId(), manifestation4, manifestation4.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer4.getId(), buyer4, TicketStatus.Reserved,
+				TicketType.Regular);
+		Ticket ticket6 = new Ticket("UzZxSs156B", manifestation1.getId(), manifestation1, manifestation1.getEventDate(),
+				manifestation1.getRegularTicketPrice(), buyer4.getId(), buyer4, TicketStatus.Reserved,
+				TicketType.Regular);
+		Ticket ticket7 = new Ticket("x8QY6WvhQk", manifestation1.getId(), manifestation2, manifestation2.getEventDate(),
+				manifestation1.getRegularTicketPrice() * TicketType.FanPit.getModifier(), buyer4.getId(), buyer4,
+				TicketStatus.Canceled, TicketType.FanPit);
+		Ticket ticket8 = new Ticket("IC9aeB4E7j", manifestation1.getId(), manifestation3, manifestation3.getEventDate(),
+				manifestation1.getRegularTicketPrice() * TicketType.Vip.getModifier(), buyer4.getId(), buyer4,
+				TicketStatus.Reserved, TicketType.Vip);
+		
+		ticketRepository.create(ticket1);
+		ticketRepository.create(ticket2);
+		ticketRepository.create(ticket3);
+		ticketRepository.create(ticket4);
+		ticketRepository.create(ticket5);
+		ticketRepository.create(ticket6);
+		ticketRepository.create(ticket7);
+		ticketRepository.create(ticket8);
+
 	}
-	
-	
+
 }
