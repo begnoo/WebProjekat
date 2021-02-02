7f13d9066d7ad01284789efd815f9c4ad26340f4
Panic <panic.milos99@gmail.com>
2021-02-02 17:33:28 +0100

    feat: implemented validation for manifestations.

--- WebContent/static/js/components/forms/manifestation/manifestationForm.js
+++ WebContent/static/js/components/forms/manifestation/manifestationForm.js
@@ -3,59 +3,60 @@ Vue.component("manifestation-form", {
         <form>
             <div class="form-group">
                 <div class="mb-3">
-                    <label for="textField" class="form-label">Name: </label>
-                    <input type="text" class="form-control" id="textField" v-model="value.name">
+                    <label :for="getId('ManifestationName')" class="form-label">Name: </label>
+                    <input type="text" class="form-control" :id="getId('ManifestationName')" v-model="value.name">
                 </div>
             </div>
             <div class="form-group">
                 <div class="mb-3">
-                    <label for="seatsField" class="form-label">Seats: </label>
-                    <input type="number" class="form-control" id="seatsField" v-model="value.seats">
+                    <label :for="getId('ManifestationSeats')" class="form-label">Seats: </label>
+                    <input type="number" class="form-control" :id="getId('ManifestationSeats')" v-model="value.seats">
                 </div>
             </div>
             <div class="form-group">
                 <div class="mb-3">
-                    <label for="priceField" class="form-label">Ticket Price: </label>
-                    <input type="number" class="form-control" id="priceField" v-model="value.regularTicketPrice">
+                    <label :for="getId('ManifestationPrice')" class="form-label">Ticket Price: </label>
+                    <input type="number" class="form-control" :id="getId('ManifestationPrice')" v-model="value.regularTicketPrice">
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-8">
                     <div class="mb-3">
-                        <label for="eventDateField" class="form-label">Event Start Date: </label>
-                        <input type="date" class="form-control" id="eventDateField" v-model="value.eventDate">
+                        <label :for="getId('ManifestationStartDate')" class="form-label">Event Start Date: </label>
+                        <input type="date" class="form-control" :id="getId('ManifestationStartDate')" v-model="value.eventDate">
                     </div>
                 </div>
                 <div class="form-group col-md-4">
                     <div class="mb-3 mt-2">
-                        <label for="eventTimeField" class="form-label"></label>
-                        <input type="time" class="form-control" id="eventTimeField" v-model="value.eventTime">
+                        <label class="form-label"></label>
+                        <label :for="getId('ManifestationStartTime')" class="form-label" hidden>Event Start Time: </label>
+                        <input type="time" class="form-control" :id="getId('ManifestationStartTime')" v-model="value.eventTime">
                     </div>
                 </div>
             </div>
             <div class="form-row">
                 <div class="form-group col-md-8">
                     <div class="mb-3">
-                        <label for="eventEndDateField" class="form-label">Event End Date: </label>
-                        <input type="date" class="form-control" id="eventEndDateField" v-model="value.eventEndDate">
+                        <label :for="getId('ManifestationEndDate')" class="form-label">Event End Date: </label>
+                        <input type="date" class="form-control" :id="getId('ManifestationEndDate')" v-model="value.eventEndDate">
                     </div>
                 </div>
                 <div class="form-group col-md-4">
                     <div class="mb-3 mt-2">
-                        <label for="eventEndTimeField" class="form-label"></label>
-                        <input type="time" class="form-control" id="eventEndTimeField" v-model="value.eventEndTime">
+                    	<label class="form-label"></label>
+                        <label :for="getId('ManifestationEndTime')" class="form-label" hidden>Event End Time: </label>
+                        <input type="time" class="form-control" :id="getId('ManifestationEndTime')" v-model="value.eventEndTime">
                     </div>
                 </div>
             </div>
             <div class="form-group">
-                <label for="typeDataSelect">Type:</label>
-                <select id="typeDataSelect" class="form-control" v-model="value.type">
+                <label :for="getId('ManifestationType')">Type:</label>
+                <select :id="getId('ManifestationType')" class="form-control" v-model="value.type">
                     <option v-for="type in types" :key="type" :id="type">{{type}}</option>
                 </select>
             </div>
             <div class="form-group">
-                <label for="locationField">Location:</label>
-                <location-combo-box v-on:location-value-change="updateLocationId" :initialLocationId="value.locationId"></location-combo-box>
+                <location-combo-box :idPrefix='idPrefix' v-on:location-value-change="updateLocationId" :initialLocationId="value.locationId"></location-combo-box>
             </div>
 
 			<manifestation-image-form :manifestationId="value.id" 
@@ -63,7 +64,7 @@ Vue.component("manifestation-form", {
 									  v-on:update-success="manifestation => this.$emit('update-success', manifestation)"></manifestation-image-form>
         </form>
     `,
-    props: ["value", "trigger"],
+    props: ["value", "trigger", "idPrefix"],
 
 	data: function(){
 		return {
@@ -78,6 +79,10 @@ Vue.component("manifestation-form", {
     },
 	
     methods: {
+		getId: function(id) {
+			return this.idPrefix + id;
+		},
+		
 		emitSubmit: function(event){
 			this.$emit('submit-value', event);
 		},
