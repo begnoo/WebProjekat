fb040a7f6c10b8ba9f3b5b3c7dc32986481b85ed
Panic <panic.milos99@gmail.com>
2021-02-04 22:14:15 +0100

    feat: added interceptor to catch 401 and logout user.

---
+++ WebContent/static/js/axiosInterceptors.js
@@ -0,0 +1,21 @@
+const UNAUTHORIZED = 401;
+
+axios.interceptors.response.use(
+	response => response,
+	error => {
+		const {status} = error.response;
+		if (status === UNAUTHORIZED) {
+			logoutUser();
+	    }
+
+		return Promise.reject(error);
+	}
+);
+
+function logoutUser() {
+	window.localStorage.removeItem("loggedUser");
+	window.localStorage.removeItem("shoppingCart");
+	store.state.userLoggedIn = false;
+	store.state.loggedUsername = null;
+	router.replace("/")
+}
